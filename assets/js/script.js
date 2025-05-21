4f55a6bf608dec8e98e0f328133458c84c0f89a1
Rick1ra <little.henrique8@gmail.com>
2025-05-20 23:28:10 -0300

    porra

--- assets/js/script.js
+++ assets/js/script.js
@@ -314,16 +314,3 @@ function init() {
 }
 
 init();
-// Função de mostrar e ocultar senha
-
-//to tentando fazer o modal abrir mas não está funcionando
-    const botoesVerMais = document.querySelectorAll(".btn-default.btn-ver-mais");
-
-    botoesVerMais.forEach(botao => {
-        botao.addEventListener("click", () => {
-            const modal = botao.parentElement.querySelector(".modal-restaurante");
-            if (modal) {
-                modal.showModal();
-            }
-        });
-    });
